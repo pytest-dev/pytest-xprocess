40900be318498301ba48c1950d518f5e2116f553
northernSage <gfvante@gmail.com>
2020-10-10 18:53:16 -0300

    create BaseTests parent class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,77 @@
+import socket
 import sys
+from abc import ABC
+from abc import abstractmethod
 from pathlib import Path
 
 import pytest
 
 from xprocess import ProcessStarter
 
+pytest_plugins = "pytester"
+
 
 @pytest.fixture
-def test_server(xprocess):
-    python_executable = sys.executable
+def make_runner(xprocess, request):
+    def _runner(start_pattern, proc_name, port):
+        class Starter(ProcessStarter):
+            pattern = start_pattern
+            server_path = Path(__file__).parent.joinpath("server.py").absolute()
+            args = [sys.executable, "-u", server_path, port]
 
-    class Starter(ProcessStarter):
-        pattern = "started"
-        server_path = Path(__file__).parent.joinpath("server.py").absolute()
-        args = [python_executable, server_path, "-u"]
+        logfile = xprocess.ensure(proc_name, Starter)
+        return logfile
 
-    xprocess.ensure("test_server", Starter)
-    yield
-    xprocess.getinfo("test_server").terminate()
+    return _runner
+
+
+@pytest.fixture
+def make_terminator(xprocess):
+    def _terminator(name):
+        proc = xprocess.getinfo(name)
+        if proc.isrunning():
+            print(f"process {name} (PID {proc.pid}), running, terminating...\n")
+            proc.terminate()
+
+    return _terminator
+
+
+class BaseTests(ABC):
+
+    HOST = "localhost"
+
+    @property
+    def port(self):
+        raise NotImplementedError
+
+    @port.setter
+    @abstractmethod
+    def port(self):
+        raise NotImplementedError
+
+    @property
+    def data(self):
+        raise NotImplementedError
+
+    @data.setter
+    @abstractmethod
+    def data(self):
+        raise NotImplementedError
+
+    @pytest.fixture(autouse=True)
+    def get_server(self, make_runner):
+        self._serve = make_runner
+
+    @pytest.fixture(autouse=True)
+    def get_terminator(self, make_terminator):
+        self._terminator = make_terminator
+
+    def test_request_response(self):
+        print("running test_base_class...")
+        self._serve("2 , % /.%,@%@._%%# #/%/ %\n", "server", 6777)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((self.HOST, self.port))
+            sock.sendall(bytes(self.data, "utf-8"))
+            received = str(sock.recv(1024), "utf-8")
+            assert received == self.data.upper()
+        self._terminator("server")
--- tests/server.py
+++ tests/server.py
@@ -1,4 +1,3 @@
-import os
 import socketserver
 import sys
 from multiprocessing import Process
@@ -10,9 +9,45 @@ class TestHandler(socketserver.StreamRequestHandler):
     The request handler class for the test server.
     """
 
-    def __init__(self):
-        self.count = 0
-        self.fork_children()
+    count = 0
+
+    def handle(self):
+        print("handling stuff...\n")
+        while True:
+            line = self.rfile.readline()
+            if not line:
+                break
+            print(f"received: {line}, request counter: {self.count}\n")
+            self.request.sendall(line.upper())
+            self.count += 1
+
+
+class TestServer(socketserver.TCPServer):
+    """ This server class is used for testing only"""
+
+    allow_reuse_address = True
+
+    def print_test_patterns(self):
+        self.spam_blank_lines()
+        self.spam_complex_strings()
+        self.spam_non_ascii()
+        print("started\n")
+        # self.fork_children()
+
+    def spam_non_ascii(self):
+        """non-ascii characters must be supported"""
+        for _ in range(5):
+            print("Ê�æ�pP��çîöē�P��adåráøū")
+
+    def spam_complex_strings(self):
+        """Special/control characters should not cause problems"""
+        for i in range(5):
+            print(f"{i} , % /.%,@%@._%%# #/%/ %\n")
+
+    def spam_blank_lines(self):
+        """Blank lines should be igored by xprocess"""
+        for _ in range(100):
+            print("\n")
 
     def fork_children(self):
         """forks multiple children for testing process tree termination"""
@@ -24,54 +59,9 @@ class TestHandler(socketserver.StreamRequestHandler):
         for _ in range(5):
             Process(target=_wait).start()
 
-    def response(self):
-        return os.environ.get("RESPONSE", "1").encode("utf8")
-
-    def handle(self):
-        while True:
-            line = self.rfile.readline()
-            if not line:
-                break
-            sys.stderr.write(f"{self.count} {line}\n")
-
-            sys.stderr.flush()
-
-            self.request.sendall(self.response())
-            self.count += 1
-
-
-class TestServer(socketserver.TCPServer):
-    """ This server class is used for testing only"""
-
-    allow_reuse_address = True
-
-    def start_serving(self):
-        self.spam_blank_lines()
-        self.spam_complex_strings()
-        self.spam_non_ascii()
-
-        sys.stderr.flush()
-
-        sys.stderr.write("started\n")
-        self.serve_forever()
-
-    def spam_non_ascii(self):
-        """non-ascii characters must be supported"""
-        for _ in range(5):
-            sys.stderr.write("Ê�æ�pP��çîöē�P��adåráøū")
-
-    def spam_complex_strings(self):
-        """Special/control characters should not cause problems"""
-        for i in range(5):
-            sys.stderr.write(f"{i} , % /.%,@%@._%%# #/%/ %\n")
-
-    def spam_blank_lines(self):
-        """Blank lines should be igored by xprocess"""
-        for _ in range(100):
-            sys.stderr.write("\n")
-
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", int(sys.argv[1])
     with TestServer((HOST, PORT), TestHandler) as server:
-        server.start_serving()
+        server.print_test_patterns()
+        server.serve_forever()
