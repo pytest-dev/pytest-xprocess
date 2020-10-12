717849044f637d2f4f9a5d2ebe780a0256a7b3f7
northernSage <gfvante@gmail.com>
2020-10-12 16:43:35 -0300

    add tests for process termination

--- tests/test_process_termination.py
+++ tests/test_process_termination.py
@@ -15,6 +15,41 @@ class TestProcessTermination(Test):
         assert proc_info.isrunning()
         children = psutil.Process(proc_info.pid).children()
         assert len(children) >= 1
-        self.terminate(proc_name)
+        assert self.terminate(proc_name)
         for child in children:
             assert not child.is_running()
+
+    @pytest.mark.parametrize("port,proc_name", [(6777, "s1"), (6778, "s2")])
+    def test_terminate_no_pid(self, port, proc_name):
+        self.start_server("started", proc_name, port)
+        proc_info = self.get_info(proc_name)
+        pid, proc_info.pid = proc_info.pid, None
+        print("**proc_info.pid: ", proc_info.pid)
+        # call terminate through XProcessInfo instance
+        # with pid=None to test edge case
+        assert proc_info.terminate() == 0
+        proc_info.pid = pid
+        assert self.terminate(proc_name) == 1
+
+    @pytest.mark.parametrize("port,proc_name", [(6777, "s1"), (6778, "s2")])
+    def test_terminate_only_parent(self, port, proc_name):
+        self.start_server("started", proc_name, port)
+        proc_info = self.get_info(proc_name)
+        children = psutil.Process(proc_info.pid).children()
+        assert len(children) >= 1
+        assert self.terminate(proc_name, kill_proc_tree=False) == 1
+        assert not proc_info.isrunning()
+        for p in children:
+            try:
+                p.terminate()
+            except Exception:
+                pass
+
+    @pytest.mark.parametrize("port,proc_name", [(6777, "s1"), (6778, "s2")])
+    def test_sigkill_after_failed_sigterm(self, port, proc_name):
+        # explicitly tell xprocess_starter fixture to make
+        # server instance ignore SIGTERM
+        self.start_server("started", proc_name, port, ignore_sigterm=True)
+        # since terminate with sigterm will fail, set a lower
+        # timeout before sending sigkill so tests won't take too long
+        assert self.terminate(proc_name, timeout=1) == 1
