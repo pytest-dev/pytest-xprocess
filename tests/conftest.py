d9114be024c736c600b28181a02a99d725c33fe1
Gabriel Fioravante <gfvante@gmail.com>
2021-02-28 00:40:23 -0300

    Fix ResourceWarning due to mishandled Popen instance and leaked file handles(#61)
    * add changelog entry
    
    * add wait call on popen to __dell__
    
    * wait on popen instances and clean up file handles
    
    * pass in file path instead of file handle
    
    * convert paths to strings for python 3.5 compatibility
    
    * add tests for ResourceWarnings
    
    * cleanup file handles in __dell__
    
    * remove hardcoded timeout
    
    * move cleanup logic to pytest_unconfigure hook
    
    * adjusted timeout so function workflow test does not hang
    
    * remove debug code
    
    * turn xprocess into a context manager
    
    * move private variable to pytest_configure hook
    
    * CHANGELOG.rst
    
    * update repo maintainer badge

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,19 @@
+import pytest
+
+from xprocess import ProcessStarter
+
 pytest_plugins = "pytester"
+
+
+@pytest.fixture
+def example(xprocess):
+    """fixture for testing ResourceWarnings
+    in test_resource_cleanup.py module"""
+
+    class Starter(ProcessStarter):
+        pattern = "foo"
+        args = ("sh", "-c", "echo foo; sleep 10; echo bar")
+
+    xprocess.ensure("example", Starter)
+    yield
+    xprocess.getinfo("example").terminate()
---
+++ tests/test_resource_cleanup.py
@@ -0,0 +1,10 @@
+def test_0(example):
+    pass
+
+
+def test_1(example):
+    pass
+
+
+def test_2(example):
+    pass
