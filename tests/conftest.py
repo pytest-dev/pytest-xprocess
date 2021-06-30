9da52c0a715d3284fb97204e75af451221de06ea
northernSage <gfvante@gmail.com>
2021-06-30 20:29:20 -0300

    remeve unecessary yield

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,4 +27,4 @@ def tcp_port():
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
         s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        yield s.getsockname()[1]
+        return s.getsockname()[1]
