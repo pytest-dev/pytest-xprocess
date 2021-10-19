ee00da74cb1fce297fa9b616d6103decf1f051ba
northernSage <gfvante@gmail.com>
2021-10-19 13:25:18 -0300

    turn xprocess into package

---
+++ xprocess/__init__.py
@@ -0,0 +1,13 @@
+from .xprocess import ProcessStarter
+from .xprocess import XProcess
+from .xprocess import XProcessInfo
+from .xprocess import XProcessResources
+
+__all__ = [
+    "ProcessStarter",
+    "XProcess",
+    "XProcessResources",
+    "XProcessInfo",
+]
+
+__version__ = "0.18.1"
