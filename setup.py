3a18359a6c5e6f7adf44248a81f15235a275f398
Gabriel Fioravante <gfvante@gmail.com>
2023-01-01 10:21:54 -0300

    Drop scm versioning and update releasing steps (#124)
    * update releasing steps
    
    * drop scm versioning
    
    * remove broken deploy action

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="pytest-xprocess",
-        use_scm_version=True,
         # this is for GitHub's dependency graph
         install_requires=["pytest>=2.8", "psutil", "py"],
     )
