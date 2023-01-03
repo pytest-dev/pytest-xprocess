9710f832ec423e31837715d59ee961f2b1047e6a
northernSage <gfvante@gmail.com>
2023-01-03 10:59:49 -0300

    add scp as dep

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="pytest-xprocess",
+        use_scm_version=True,
         # this is for GitHub's dependency graph
         install_requires=["pytest>=2.8", "psutil", "py"],
     )
