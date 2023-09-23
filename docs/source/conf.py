aec1fcf3cb818b15e76ade951244d6a11382217b
northernSage <gfvante@gmail.com>
2023-09-23 12:19:29 -0300

    release 0.23.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = "2021, Holger Krekel"
 author = "Holger Krekel"
 
 # The full version, including alpha/beta/rc tags
-release = "0.18.1"
+release = "0.23.0"
 
 
 # -- General configuration ---------------------------------------------------
